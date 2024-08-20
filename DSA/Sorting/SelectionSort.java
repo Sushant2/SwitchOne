1861f66e45b3111bdd652db977eb33f796dbf626
Sushant Gupta <sushant.gupta@franconnect.com>
2024-08-20 23:09:44 +0530

    selection sort

---
+++ DSA/Sorting/SelectionSort.java
@@ -0,0 +1,14 @@
+// selection sort program
+
+public static void selectionSort(int[] arrayToSort){
+    for(int index = 0;index<n-1;index++){
+        int minIndex = index;
+        for(int secondIndex = index+1;secondIndex<n;secondIndex++){
+            if(arrayToSort[minIndex] > arrayToSort[secondIndex])
+                minIndex = secondIndex;
+        }
+        swap(int[] arrayToSort, int minIndex, int index);
+    }
+}
+
+//time compl - O(n^2)
