41dcbae34306a136d230cc5fd2d9d54d37457a19
Sushant Gupta <sushant.gupta@franconnect.com>
2024-08-22 00:18:34 +0530

    bubble sort optimised code

--- DSA/Sorting/BubbleSort.java
+++ DSA/Sorting/BubbleSort.java
@@ -9,4 +9,21 @@ public static void bubbleSort(int[] arrayToSort){
     }
 }
 
-//time compl - O(n^2)
+//time compl - O(n^2) - worst & average
+//time compl - O(n) - best when already sorted
+
+// optimised code for best case
+
+public static void bubbleSort(int[] arrayToSort){
+    for(int index = arraySize - 1;index >= 0;index--){
+        boolean didSwaps = false;
+        for(int secondIndex = 0;secondIndex<index;secondIndex++){
+            if(arrayToSort[secondIndex] > arrayToSort[secondIndex+1]){
+                swap(arrayToSort, secondIndex, secondIndex+1);
+                didSwaps = true;
+            }
+        }
+        if(didSwaps == false)
+            break;
+    }
+}
