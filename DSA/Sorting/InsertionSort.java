8c91c3567a8be202e16245bf14a15a5f133a46fb
Sushant Gupta <sushant.gupta@franconnect.com>
2024-08-23 23:22:26 +0530

    Insertion Sort

---
+++ DSA/Sorting/InsertionSort.java
@@ -0,0 +1,11 @@
+// Insertion sort program
+
+public static void insertionSort(int[] arrayToSort){
+    for(int index = 0;index<=n-1;index++){
+        int secondIndex = index;
+        while(secondIndex > 0 && arrayToSort[secondIndex] < arrayToSort[secondIndex-1]){
+            swap(arrayToSort, secondIndex, secondIndex-1);
+            j--;
+        }
+    }
+}
