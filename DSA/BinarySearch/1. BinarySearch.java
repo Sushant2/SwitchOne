35343199c216cefc4c41d5d90e2a547cc0005792
Sushant Gupta <er.sushant.2@gmail.com>
2024-11-14 17:52:57 +0530

    recursive binary search

--- DSA/BinarySearch/1. BinarySearch.java
+++ DSA/BinarySearch/1. BinarySearch.java
@@ -1,5 +1,6 @@
 //program to return index if target present in sorted array else return -1
 
+//Iterative approach
 class Solution {
     public int search(int[] nums, int target) {
         int low = 0, high = nums.length-1;
@@ -22,3 +23,23 @@ class Solution {
 //space compl - O(1)
 
 
+class Solution {
+    public int search(int[] nums, int target) {
+        return binarySearchRec(nums, 0, nums.length-1, target);
+    }
+    public int binarySearchRec(int[] arr, int low, int high, int x){
+        if(low > high)
+            return -1;
+        int mid = (low + (high-low)/2);
+        if(arr[mid] == x)
+            return mid;
+        else if(arr[mid] < x)
+            return binarySearchRec(arr, mid+1, high, x);
+        else
+            return binarySearchRec(arr, low, mid-1, x);
+    }
+}
+
+
+//time compl - O(logn)
+//space compl - O(1)
