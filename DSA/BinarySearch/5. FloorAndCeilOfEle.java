eb5632011d2c52129d5555dc7bda5b7414dcadbd
Sushant Gupta <er.sushant.2@gmail.com>
2024-12-06 00:29:32 +0530

    floor and ceil of ele

--- DSA/BinarySearch/5. FloorAndCeilOfEle.java
+++ DSA/BinarySearch/5. FloorAndCeilOfEle.java
@@ -2,12 +2,17 @@
 //floor(ele) = largest ele <= x
 //ceil(ele) = smallest ele >= x
 
+//to find ceil - just use lower bound
+//to find floor - tweak the lower bound little bit
+
 public static int[] getFloorAndCeil(int[] a, int n, int x) {
       // Write your code here.
       int floor = -1;
       int ceil = -1;
       int low = 0;
       int high = n-1;
+
+      //finding floor
       while(low <= high){
         int mid = low + (high-low)/2;
         if(a[mid] <= x){
@@ -20,6 +25,8 @@ public static int[] getFloorAndCeil(int[] a, int n, int x) {
 
       low = 0;
       high = n-1;
+
+      //findind ceil
       while(low <= high){
         int mid = low + (high-low)/2;
         if(a[mid] >= x){
