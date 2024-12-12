a3348369e1092fd4b6f6bb5c0218d371415912ad
Sushant Gupta <er.sushant.2@gmail.com>
2024-12-13 00:25:26 +0530

    firstandlastocc binarysearch

--- DSA/BinarySearch/6. FirstAndLastOccurence.java
+++ DSA/BinarySearch/6. FirstAndLastOccurence.java
@@ -65,3 +65,54 @@ class Solution {
         return new int[]{lb, upperBound(nums, n, target)};
     }
 }
+
+
+//Another approach - using simple binary search
+//finding first occ & then last occ (ont by one)
+
+class Solution {
+    public int findFirst(int[] arr, int k){
+        int first = -1;
+        int low = 0, high = arr.length-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(arr[mid] == k){
+                first = mid;
+                high = mid-1;
+            }else if(arr[mid] > k)
+                high = mid-1;
+            else
+                low = mid+1;
+        }
+        return first;
+    }
+    public int findLast(int[] arr, int k){
+        int last = -1;
+        int low = 0, high = arr.length-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(arr[mid] == k){
+                last = mid;
+                low = mid+1;
+            }else if(arr[mid] > k)
+                high = mid-1;
+            else
+                low = mid+1;
+        }
+        return last;
+    } 
+    public int[] searchRange(int[] nums, int target) {
+        if(nums.length == 0)
+            return new int[]{-1, -1};
+            
+        int first = findFirst(nums, target);
+        if(first  == -1)
+            return new int[]{-1, -1};
+        int last = findLast(nums, target);
+        return new int[]{first, last};
+    }
+}
+
+
+//time compl - O(2logn)
+//space compl - O(1)
