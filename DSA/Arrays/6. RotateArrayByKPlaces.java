2a202ec000acebd56e9a9be08e5a4c739545e27e
Sushant Gupta <er.sushant.2@gmail.com>
2024-12-31 16:39:49 +0530

    rotate array by k places using recursion

--- DSA/Arrays/6. RotateArrayByKPlaces.java
+++ DSA/Arrays/6. RotateArrayByKPlaces.java
@@ -43,3 +43,34 @@ class Solution {
 }
 //Time Compl - O(n)
 //Space Compl - O(n)
+
+
+//Using the observation ki each part ko reverse krne se, rotate ho jaa rha h array
+//so using recursion - which doesn't require any extra space
+
+class Solution {
+    public void reverse(int[] arr, int left , int right){
+        if(left >= right)
+            return;
+        //swap
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+
+        reverse(arr, left+1, right-1);
+    }
+    public void rotate(int[] nums, int k) {
+        int size = nums.length;
+        int[] temp = new int[size];
+        
+        if(size==1) return;
+        k = k%size;
+
+        reverse(nums, 0, size-k-1);
+        reverse(nums, size-k, size-1);
+        reverse(nums, 0, size-1);
+    }
+}
+
+//time compl - O(n)
+//space compl - O(1)
