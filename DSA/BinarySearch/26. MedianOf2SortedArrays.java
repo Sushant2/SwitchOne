1a783db8792f3f26ec415948ce286eab20837543
Sushant Gupta <er.sushant.2@gmail.com>
2025-02-12 01:37:12 +0530

    binary search approach

--- DSA/BinarySearch/26. MedianOf2SortedArrays.java
+++ DSA/BinarySearch/26. MedianOf2SortedArrays.java
@@ -91,3 +91,48 @@ class Solution {
 //time compl - O(m+n)
 //space compl - O(1)
 
+
+//using binary search
+//observe a symmatry from median
+//how many element to take from arr1 and arr2 to make the correct left half of symmetry
+//range from 0 to min(arr1.length, arr2.length)
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+       int m = nums1.length;
+       int n = nums2.length;
+       //if first array is bigger
+       if(m > n) return findMedianSortedArrays(nums2, nums1); 
+       int len = m+n;
+       int left = (len+1)/2;
+       int low = 0, high = m;
+       while(low <= high){
+        int mid1 = (low + high) >> 1; //how many from first array
+        int mid2 = left - mid1; //how many from 2nd
+        //values of l1, l2, r1, r2
+        //on left side, if we pick 0 ele from arr1, so the l1 should be intmin
+        //on left side, if we pick 0 ele from arr2, so the l2 should be intmin
+        //on right side, if we pick 0 ele from arr1, so r1 should be intmax
+        //on right side, if we pick 0 ele from arr2, so r2 should be intmax
+        int l1 = (mid1 > 0) ? nums1[mid1 - 1] : Integer.MIN_VALUE;
+        int l2 = (mid2 > 0) ? nums2[mid2 - 1] : Integer.MIN_VALUE;
+        int r1 = (mid1 < m) ? nums1[mid1] : Integer.MAX_VALUE;
+        int r2 = (mid2 < n) ? nums2[mid2] : Integer.MAX_VALUE;
+        //valid case
+        if(l1 <= r2 && l2 <= r1){
+            //for odd
+            if(len % 2 != 0)
+                return Math.max(l1, l2);
+            return ((double) (Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
+        }else if(l1 > r2)
+            high = mid1-1;
+        else
+            low = mid1+1;
+       }
+
+       return 0.0;
+    }
+}
+
+//time compl - O(log(m+n)
+//space compl - O(1)
