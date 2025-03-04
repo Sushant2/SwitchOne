2a0bffe70feeb054652fb6e1e6c30ae94b6abf6d
Sushant Gupta <er.sushant.2@gmail.com>
2025-03-05 00:34:05 +0530

    kth element of 2 sorted arrays

--- DSA/BinarySearch/27. KthElementOf2SortedArrays.java
+++ DSA/BinarySearch/27. KthElementOf2SortedArrays.java
@@ -71,3 +71,36 @@ class Solution {
 //time compl - O(m+n)
 //space compl - O(1)
 
+//optimised approach
+//using binary search - same as median of 2 sorted arrays
+
+class Solution {
+    public int kthElement(int a[], int b[], int k) {
+        // code here
+        int m = a.length;
+        int n = b.length;
+        if(m > n)
+            return kthElement(b, a, k);
+        
+        int low = Math.max(0, k-n), high = Math.min(k, m);
+        while(low <= high){
+            int cut1 = low + (high-low)/2;
+            int cut2 = k - cut1;
+            
+            int l1 = cut1 == 0 ? Integer.MIN_VALUE : a[cut1-1];
+            int l2 = cut2 == 0 ? Integer.MIN_VALUE : b[cut2-1];
+            int r1 = cut1 == m ? Integer.MAX_VALUE : a[cut1];
+            int r2 = cut2 == n ? Integer.MAX_VALUE : b[cut2];
+            
+            if(l1 <= r2 && l2 <= r1){
+                return Math.max(l1, l2);
+            }else if(l1 > r2){
+                high = cut1-1;
+            }else{
+                low = cut1+1;
+            }
+        }
+        
+        return -1;
+    }
+}
