7f5e316169d2ca0b426a221997e918d4e7d98c0e
Sushant Gupta <er.sushant.2@gmail.com>
2026-04-23 06:21:19 +0530

    Singleton Design Pattern - Lazy Initialization(Thread Safe)

---
+++ LLD/DesignPatterns/Singleton/SingletonThreadSafe.java
@@ -0,0 +1,14 @@
+package LLD.DesignPatterns.Singleton;
+
+public class SingletonThreadSafe {
+    private static SingletonThreadSafe instance;
+
+    private SingletonThreadSafe(){}
+
+    public static synchronized SingletonThreadSafe getInstance(){
+        if(instance == null){
+            instance = new SingletonThreadSafe();
+        }
+        return instance;
+    }
+}
